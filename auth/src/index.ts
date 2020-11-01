93fc265369234be50e41f3ac5521a8296e24813b
KANSO Abdallah <abdallah.kanso@unine.ch>
2020-11-01 23:14:37 +0100

    force build

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -18,7 +18,7 @@ const start = async () => {
 
   app.listen(3000, () => {
     console.log(`Auth process.env.NODE_ENV:${process.env.NODE_ENV}`)
-    console.log('Listening on port 3000   ! ! !')
+    console.log('Listening on port 3000  ! ! !')
   })
 }
 
